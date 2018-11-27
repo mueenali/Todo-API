0cebfe4ea648133f80a02f08c541e8a1e4f8231e
Mueen <moenali159@gmail.com>
2018-11-27 11:47:48 +0800

    ADD POST Users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,65 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const passwordValidator = require('password-validator');
 var Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+var schema = new passwordValidator();
+schema.has().uppercase();
+
 var UserSchema = new Schema({
     email: {
         type : String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique : true,
+        validate :{
+            validator : validator.isEmail,
+            message : `{Value} is not a valid email`
+        }
+    },
+    password : {
+        type : String,
+        required: true,
+        minlength: 6,
+        validate:{
+            validator : (val) =>{
+                return schema.validate(val);
+            },
+            message: 'Password does not contain any uppercase letters'
+        }
+    },
+    tokens :[{
+        access :{
+            type : String,
+            required : true,
+
+        },
+        token :{
+            type : String,
+            required : true
+        }
+    }]
 });
+
+UserSchema.methods.toJSON = function (){
+    var user = this;
+    var userObject = user.toObject();
+    return _.pick(userObject,['_id','email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(),access},'abcd').toString();
+  user.tokens = user.tokens.concat([{access,token}]);
+  return user.save().then(() =>{
+      return token;
+  })
+};
+
+
 
 var User = mongoose.model('User',UserSchema);
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ const app = express();
 const port = process.env.PORT;
 app.use(bodyParser.json());
 
-
 app.post('/todos',(req,res) =>{
     var todo = new Todo({
         text : req.body.text
@@ -86,6 +85,22 @@ app.patch('/todos/:id',(req,res) =>{
         res.status(400).send();
     })
 });
+
+
+//
+
+app.post('/users',(req,res) =>{
+    var body =_.pick(req.body,['email','password']);
+    var user = new User(body);
+    user.save().then(() =>{
+        return user.generateAuthToken();
+    }).then((token) =>{
+        res.header('x-auth',token).send(user);
+    }).catch((err) =>{
+        res.status(400).send(err);
+    })
+});
+
 
 app.listen(port,() =>{
     console.log(`Started on port ${port}`);
